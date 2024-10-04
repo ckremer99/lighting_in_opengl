27d92902ce575c276f07332afb521ee7bbe78600
ckremer99 <ckremer99@outlook.com>
2024-10-04 14:20:51 -0700

    Initial Commit

---
+++ lighting_in_opengl/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lighting_in_opengl
+//
+//  Created by Cody Kremer on 10/4/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
